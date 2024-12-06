9ef443bded9967b3676aa2a82361b27d5fde5806
JGabela <josemiguelgabela101@gmail.com>
2024-12-06 07:58:50 -0500

    Avances

--- funciones.py
+++ funciones.py
@@ -11,4 +11,86 @@ def bancopalarbras(): #Escoge una palabra para el juego de manera aleatoria
     palabra = banco.get(random.randint(1, 5)) #
     return palabra
 
+#Imprime visuales para representar los fallos del usuario
+#Al llegar a 6 fallos pierde
+def visuales(fallos):
+    if fallos == 0:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    /|\\')
+        print(' |     |')
+        print(' |    / \\')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 1:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    ')
+        print(' |     ')
+        print(' |    ')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 2:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |     |')
+        print(' |     |')
+        print(' |    ')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 3:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    /|')
+        print(' |     |')
+        print(' |    ')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 4:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    /|\\')
+        print(' |     |')
+        print(' |    ')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 5:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    /|\\')
+        print(' |     |')
+        print(' |    / ')
+        print(' |     ')
+        print(' ---------')
+    elif fallos == 6:
+        print(' +_____+')
+        print(' |     |')
+        print(' |     0')
+        print(' |    /|\\')
+        print(' |     |')
+        print(' |    / \\')
+        print(' |     ')
+        print(' ---------')
+
+
+
+
+
+def denuevo(jugar): #Pregunta si el usuario quiere seguir jugando o no
+    a = input('Quiere seguir jugando?: ')
+    if a == 'si':
+        jugar = True
+    elif a == 'Si':
+        jugar = True
+    elif a == 'no':
+        jugar = False
+    elif a == 'No':
+        jugar = False
+    return jugar
 
--- main.py
+++ main.py
@@ -3,23 +3,22 @@
 import funciones
 
 print('EL AHORCADO')
-jugar = True
+jugar = True #La variable se la usa para el loop while que corre el juego
+#Solo funciona como separadores para que se vea mejor la interfaz del juego
+print('-----------------------------------------------')
 
-while jugar is True:
+while jugar is True: #El loop sirve para volver a jugar si el usuario lo desea
 
-    respuesta = funciones.bancopalarbras()
-    espacio = '_____'
+    respuesta = funciones.bancopalarbras() #Extrae un palabra random para la partida
+    espacio = 'Palabra: _____'
     print(espacio)
+    print('')
+
+
+
 
 
 
     #Al terminar la partida se le pregunta al usuario si quiere seguir jugando o no
-    a = input('Quiere seguir jugando?')
-    if a == 'si':
-        jugar = True
-    elif a == 'Si':
-        jugar = True
-    elif a == 'no':
-        jugar = False
-    elif a == 'No':
-        jugar = False
+    jugar = funciones.denuevo(jugar)
+    print('-----------------------------------------------')
